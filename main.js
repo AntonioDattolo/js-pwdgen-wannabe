d7f27331728fd1314f8f817585a3e7c13f535212
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-16 14:51:32 +0200

    Second Commit and Finish

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstName= prompt()
+let surname= prompt()
+let favColor= prompt()
+
+let succes = `${firstName}${surname}${favColor}\#24\ `
+
+//alert(succes)
+
+document.getElementById("result").innerHTML=`SUCCES: ${succes}`
